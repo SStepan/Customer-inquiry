3a595b73b86bf0f02d0f6758e5486ae4ee5039ab
Stepan Myroniuk <myronyuk.s@gmail.com>
2019-07-21 19:49:36 +0300

    add constraints to Transaction table

--- CustomerInquiry.Database/UserData/Tables/Transaction.sql
+++ CustomerInquiry.Database/UserData/Tables/Transaction.sql
@@ -2,7 +2,12 @@
 (
 	[TransactionId] INT NOT NULL PRIMARY KEY,
 	[CustomerId] INT NOT NULL,
-	[TransactionDate] DATETIME,
-	[Amount] DECIMAL(16, 2),
+	[TransactionDate] DATETIME NOT NULL,
+	[Amount] DECIMAL(16, 2) NOT NULL,
+	[CurrencyCodeId] INT NOT NULL,
+	[TransactionStatusId] INT NOT NULL,
 
+	CONSTRAINT [FK_Transaction_Customer] FOREIGN KEY ([CustomerId]) REFERENCES [UserData].[Customer]([CustomerId]),
+	CONSTRAINT [FK_Transaction_CurrencyCode] FOREIGN KEY ([CurrencyCodeId]) REFERENCES [Installation].[CurrencyCode]([CurrencyCodeId]),
+	CONSTRAINT [FK_Transaction_TransactionStatus] FOREIGN KEY ([TransactionStatusId]) REFERENCES [Installation].[TransactionStatus]([TransactionStatusId])
 )
